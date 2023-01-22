ecf84b902bbd70e185f594cc2527537bc8d2ea9c
Vasyl <vasyl@voicett.com>
2023-01-22 17:19:55 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// webpack.config.js
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".ts", ".tsx", ".js", ".css"],
+  }
+};
