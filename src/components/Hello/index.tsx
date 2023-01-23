4a81f4e17c1ac309b11a8aac7921a32dbced63d5
Vasyl <vasyl@voicett.com>
2023-01-23 16:23:57 +0200

    Add support for css modules.

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,10 +1,10 @@
-import './index.scss'
+import styles from './index.modules.scss'
 import reactImage from './images/react-image.png'
 
 const Hello = () => {
   return (
     <>
-      <h1 className='some-class'>Hello from Julia!!!</h1>;
+      <h1 className={styles['some-class']}>Hello from Julia!!!</h1>;
       <img src={reactImage} alt='react image' />
     </>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,19 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, 'public', 'index.html'),
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "build"),
+      directory: path.join(__dirname, 'build'),
     },
     port: 3000,
   },
@@ -25,19 +25,19 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"],
+        use: ['ts-loader'],
       },
       {
         test: /\.(s(a|c)ss)$/,
-        use: [ "style-loader", "css-loader", "sass-loader" ]
+        use: ['style-loader', { loader: 'css-loader', options: { modules: true } }, 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [{ loader: 'file-loader' }]
-        },
+        use: [{ loader: 'file-loader' }],
+      },
     ],
   },
   resolve: {
-    extensions: ["*", ".ts", ".tsx", ".js", ".css", ".scss"],
-  }
-};
+    extensions: ['*', '.ts', '.tsx', '.js', '.css', '.scss'],
+  },
+}
